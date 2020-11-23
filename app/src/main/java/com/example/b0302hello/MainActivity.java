38b45364786d22a347b375338ae04a80ed8d1aaa
mlapinm <mlapin@rambler.ru>
2020-11-23 14:00:16 +0300

    11

---
+++ app/src/main/java/com/example/b0302hello/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.b0302hello;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
